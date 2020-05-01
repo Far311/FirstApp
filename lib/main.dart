606296fc557b33345685b40b02bb8d28f3cb0cc2
Far311 <fare11@mail.ru>
2020-05-02 01:52:56 +0700

    changed main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+
+
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
+
+
+
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -42,6 +49,9 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
+
+
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
